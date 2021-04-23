4187bdb4f921b705e8d0c2612eeac3b6cefba1db
embwana <emmanuelmbwana1@outlook.com>
2021-04-23 15:23:21 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitDemoOne.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        [Display(Name ="Book Name")]
+        [StringLength(50)]
+        public string BookName { get; set; }
+        [Required]
+        [Display(Name ="Author Name")]
+        [StringLength(50)]
+        public string AuthorName { get; set; }
+        [Required]
+        [Display(Name ="Book Price")]
+        public decimal Price { get; set; }
+    }
+}
