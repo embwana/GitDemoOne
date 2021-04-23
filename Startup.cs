2ca56cbf3b0afa0c93a7ec41ef3637b684bbd034
embwana <emmanuelmbwana1@outlook.com>
2021-04-23 15:45:28 +0200

    commit

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
